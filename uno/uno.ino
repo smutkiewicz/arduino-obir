bd8ec5ef24d9bd36d64960db8c8d5e2adccb3f14
smutkiewicz <smutkiewicz@gmail.com>
2019-12-20 15:03:01 +0100

    Radio & ethernet added to uno

--- uno/uno.ino
+++ uno/uno.ino
@@ -1,4 +1,6 @@
 #include <SPI.h>
+#include <RF24Network.h> 
+#include <RF24.h>
 #include <Dhcp.h>
 #include <Dns.h>
 #include <Ethernet.h>
@@ -7,8 +9,15 @@
 
 #define LEDP 9
 
-byte mac[] = { 0x00, 0xaa, 0xbb, 0xcc, 0xde, 0xf3 };
-IPAddress dev_ip(10, 0, 0, 177);
+/*
+if you change LED, req/res test with coap-client(libcoap), run following.
+coap-client -m get coap://(arduino ip addr)/light
+coap-client -e "1" -m put coap://(arduino ip addr)/light
+coap-client -e "0" -m put coap://(arduino ip addr)/light
+*/
+
+byte mac[] = { 0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02 };
+IPAddress dev_ip(192, 168, 0, 13);
 
 // CoAP client response callback
 void callback_response(CoapPacket &packet, IPAddress ip, int port);
@@ -19,6 +28,20 @@ void callback_light(CoapPacket &packet, IPAddress ip, int port);
 // UDP and CoAP class
 EthernetUDP Udp;
 Coap coap(Udp);
+
+// Radio
+RF24 radio(7, 8);
+RF24Network network(radio);
+
+int CHANNEL = 47;
+int THIS_NODE_ID = 0;
+int PEER_NODE_ID = 1;
+
+struct payload {  
+  unsigned long timestamp; // stempel czasowy  
+  int type;
+  int value;
+};
 
 // LED STATE
 bool LEDSTATE;
@@ -62,7 +85,14 @@ void callback_response(CoapPacket &packet, IPAddress ip, int port) {
 void setup() {
   Serial.begin(115200);
 
+  // Radio
+  SPI.begin(); 
+  radio.begin();
+  network.begin(CHANNEL, THIS_NODE_ID);
+
+  // Internet
   Ethernet.begin(mac, dev_ip);
+  
   Serial.print("My IP address: ");
   Serial.print(Ethernet.localIP());
   Serial.println();
@@ -92,16 +122,38 @@ void setup() {
 void loop() {
   // send GET or PUT coap request to CoAP server.
   // To test, use libcoap, microcoap server...etc
-  // int msgid = coap.put(IPAddress(10, 0, 0, 1), 5683, "light", "1");
+  //int msgid = coap.put(IPAddress(10, 0, 0, 177), 5683, "light", "1");
   //Serial.println("Send Request");
-  //int msgid = coap.get(IPAddress(10, 0, 0, 1), 5683, "time");
+  //int msgid = coap.get(IPAddress(192, 168, 0, 1), 5683, "time");
 
   //delay(1000);
   coap.loop();
+  network_loop();
 }
-/*
-if you change LED, req/res test with coap-client(libcoap), run following.
-coap-client -m get coap://(arduino ip addr)/light
-coap-client -e "1" -m put coap://(arduino ip addr)/light
-coap-client -e "0" -m put coap://(arduino ip addr)/light
-*/
+
+bool send_radio_msg(int type, int value) {
+  struct payload payload;
+  payload.timestamp = 1;
+  payload.type = type;
+  payload.value = value;
+  RF24NetworkHeader header(PEER_NODE_ID);
+  return network.write(header, &payload, sizeof(payload));
+}
+
+bool receive_radio_msg(struct payload* p) {
+  RF24NetworkHeader header(THIS_NODE_ID);
+  return network.read(header, &p, sizeof(p));
+}
+
+void network_loop() {
+  network.update(); 
+  while(network.available()) {
+    struct payload payload;
+    bool success = receive_radio_msg(&payload);
+    if (success) {
+      Serial.println("Received some stuff from peer");  
+    } else {
+      Serial.println("Ooopsie whoopsie, you've failed successfully");  
+    }
+  }
+}
