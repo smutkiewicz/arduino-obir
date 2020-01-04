d3deb744bf96b37babfc8d495e2bf1ecd30b6ebf
smutkiewicz <smutkiewicz@gmail.com>
2020-01-04 12:18:44 +0100

    Uno: commented uno.ino

--- uno/uno.ino
+++ uno/uno.ino
@@ -10,7 +10,7 @@
 struct payload_t
 {
   unsigned long timestamp; // stempel czasowy
-  short type; // typ resource'u
+  short type; // typ zasobu
   int value; // przenoszona wartość
 };
 
@@ -21,34 +21,36 @@ struct observer
   int port;
   uint8_t token[2];
   uint8_t tokenlen;
-  uint8_t counter;
-  uint8_t content_type;
+  uint8_t counter; // numer sekwencji
+  uint8_t content_type; // typ (plaintext lub json)
 };
 
 //===========================================================================================================
 // zasoby
 //===========================================================================================================
-int last_pressed = 35;
-int led_level = 950;
+int last_pressed = 35; // kod ascii ostatnio wciśniętego znaku
+int led_level = 950; // poziom światła lampki led (0-1000)
+
+// zasób opisujący pozostałe zasoby w formacie CoRE Link Format
 const String well_known = "</light>;ct=0,</keyboard>;ct=0;rt=\"obs\",</statistics>;ct=0";
 
-// dla ułatwienia konwersji dla payloadu
-char lamp[4] = "500"; // how bright the lamp is - 0 is off, 1000 is max brightness
-char keyboard = '3'; // what button was last pressed on keyboard
+// łańcuchy dla ułatwienia konwersji dla payloadu
+char lamp[4] = "500"; // 0 - lampka wyłaczona, 1000 - max poziom światła
+char keyboard = '3'; // ostatni wcisniety znak na klawiaturze
 
-// obserwator zasobu klawiatury
-observer our_observer;
+observer our_observer; // obserwator zasobu klawiatury
 
 //===========================================================================================================
 // komponenty dla potrzeb radia
 //===========================================================================================================
-const short THIS_NODE_ID = 0;
-const short PEER_NODE_ID = 1;
+const short THIS_NODE_ID = 0; // radiowe id Arduino UNO
+const short PEER_NODE_ID = 1; // radiowe id Arduino Mini Pro
 
-RF24 radio(7, 8);
+// komponenty radiowe
+RF24 radio(7, 8); 
 RF24Network network(radio);
 
-// id zasobów dla radia
+// id zasobów do komunikacji z radiem
 const short ALL = 0;
 const short GET_KEYBOARD = 1;
 const short GET_LED = 2;
@@ -58,12 +60,12 @@ const short STATS = 4;
 //===========================================================================================================
 // komponenty dla potrzeb Ethernetu i CoAP
 //===========================================================================================================
-const short MAX_BUFFER_IN = 255;
 const byte mac[] = {0x00, 0xaa, 0xbb, 0xcc, 0xde, 0xf3};
 //const byte ip[] = {169, 254, 243, 41};
 const byte ip[] = {192, 168, 137, 2};
-const short localPort = 5683;
+const short local_port = 5683;
 
+// komponenty CoAP/Ethernet
 EthernetUDP udp;
 Coap server(udp);
 
@@ -75,18 +77,18 @@ void setup()
 {
   Serial.begin(115200);
 
-  // ethernet connection
+  // konfiguracja połączenia Ethernet
   Ethernet.begin(mac, ip);
   Serial.print("ip: ");
   Serial.println(Ethernet.localIP());
-  udp.begin(localPort);
+  udp.begin(local_port);
 
-  // endpointy CoAP
+  // dodanie endpointów CoAP
   server.server(light_callback, "light");
   server.server(keyboard_callback, "keyboard");
   server.server(statistics_callback, "statistics");
   server.server(general_callback, ".well-known/core");
-  server.start(); //start the coap server
+  server.start(); // uruchom serwer coap
 
   // konfiguracja połączenia radiowego
   SPI.begin();
@@ -104,49 +106,48 @@ void loop()
 // radio
 //===========================================================================================================
 
+// ogólna funkcja do wysyłania wiadomosci do Arduino Mini
 bool radio_send_msg(short type, int value)
 {
-  // update statistics
   Serial.print("Radio send msg type ");
   Serial.println(type);
 
   payload_t payload{millis(), type, value};
-  RF24NetworkHeader header(PEER_NODE_ID);
+  RF24NetworkHeader header(PEER_NODE_ID); // nagłówek wiadomosci przez radio, adresowanie do Mini
 
-  bool success = false;
-  short retries = 1;
+  bool success = false; // czy wysłanie wiadomosci przez radio sie powiodlo
+  short retries = 1; // ilosc prób, max = 5
 
-  while (!success && retries < 6)
+  while (!success && retries < 6) // próbuj max 5 razy
   {
-    success = network.write(header, &payload, sizeof(payload));
+    success = network.write(header, &payload, sizeof(payload)); // spróbuj wysłać wiadomosć
     if (success) Serial.println("Success.");
-    else
-    {
-      retries++;
-    }
+    else retries++;
   }
 
   if (!success) Serial.println("Failure.");
   return success;
 }
 
+// ogólna funkcja do odbierania wiadomosci od Arduino Mini
 bool radio_receive_msg(payload_t* p)
 {
-  RF24NetworkHeader header(THIS_NODE_ID);
-  return network.read(header, &p, sizeof(p));
+  RF24NetworkHeader header(THIS_NODE_ID); // nagłówek wiadomosci przez radio, adresowanie do Uno
+  return network.read(header, &p, sizeof(p)); // spróbuj odebrać wiadomosć
 }
 
+// główna pętla radia
 void radio_loop()
 {
-  while (network.available())
+  while (network.available()) // sprawdź, czy nadeszła nowa wiadomosć
   {
     payload_t payload;
-    bool success = radio_receive_msg(&payload);
+    bool success = radio_receive_msg(&payload); // spróbuj odebrać wiadomosć
     if (success)
     {
       Serial.print("Radio msg, timestamp ");
       Serial.println(payload.timestamp);
-      radio_handle_payload(payload);
+      radio_handle_payload(payload); // zareaguj na poprawnie odebraną wiadomosć
     }
     else
     {
@@ -155,6 +156,7 @@ void radio_loop()
   }
 }
 
+// ogólna funkcja do reakcji na poprawnie odebraną wiadomosć przez radio
 void radio_handle_payload(payload_t payload)
 {
   switch (payload.type)
@@ -163,16 +165,17 @@ void radio_handle_payload(payload_t payload)
       Serial.print("LED lvl = ");
       Serial.println(payload.value);
 
-      led_level = payload.value;
+      led_level = payload.value; // aktualizacja wartosci
+      sprintf(lamp, "%d", led_level); // konwertuj pomocniczo wartosć
       break;
 
     case GET_KEYBOARD:
       Serial.print("Last key pressed was ");
       Serial.println(payload.value);
 
-      last_pressed = payload.value;
-      keyboard = (char) last_pressed;
-      notify();
+      last_pressed = payload.value; // aktualizacja wartosci
+      keyboard = (char) last_pressed; // konwertuj pomocniczo wartosć
+      notify(); // powiadom obserwatora, jeśli istnieje
 
       break;
 
@@ -181,6 +184,10 @@ void radio_handle_payload(payload_t payload)
       break;
   }
 }
+
+//===========================================================================================================
+// Żądania zasobów przez radio
+//===========================================================================================================
 
 void get_led()
 {
@@ -205,78 +212,96 @@ void set_led(int value)
 // Serwer CoAP
 //===========================================================================================================
 
+// funkcja do odpowiadania pakietem ACK na pakiety typu CON
+// funkcja wspiera też coap ping - puste wiadomosci CON
 void conack_callback(CoapPacket &packet, IPAddress ip, int port)
 {
   if (packet.type == COAP_CON) server.sendAck(ip, port, packet.messageid, packet.token, packet.tokenlen);
 }
 
-void light_callback(CoapPacket &packet, IPAddress ip, int port)
+// callback obsługujący żądania .well-known/core i zwracający zasób opisujący pozostałe zasoby w formacie CoRE Link Format
+void general_callback(CoapPacket &packet, IPAddress ip, int port)
 {
-  conack_callback(packet, ip, port);
-  
+  conack_callback(packet, ip, port); // obsługa CON/CoAP Ping
+
+  // obsługa żądania w zależnosci od kodu wiadomosci
   if (packet.code == COAP_GET)
   {
-    //get current value over the radio
-    get_led();
-    sprintf(lamp, "%d", led_level);
-    server.sendResponse(ip, port, packet.messageid, lamp, strlen(lamp), packet.type, COAP_CONTENT, COAP_TEXT_PLAIN, packet.token, packet.tokenlen);
-  }
-  else if (packet.code == COAP_PUT)
-  {
-    //copy payload to array
-    char p[packet.payloadlen + 1];
-    memcpy(p, packet.payload, packet.payloadlen);
-    //put null -> makes string
-    p[packet.payloadlen] = NULL;
-    String message(p);
-    set_led(atoi(p));
+    server.sendResponse(ip, port, packet.messageid, well_known.c_str(), strlen(well_known.c_str()), 
+                        packet.type, COAP_CONTENT, COAP_APPLICATION_LINK_FORMAT, packet.token, packet.tokenlen);
   }
 }
 
+// callback obsługujący żądania dotyczące lampki LED
+void light_callback(CoapPacket &packet, IPAddress ip, int port)
+{
+  conack_callback(packet, ip, port); // obsługa CON/CoAP Ping
+
+  // obsługa żądania w zależnosci od kodu wiadomosci
+  if (packet.code == COAP_GET)
+  {
+    get_led(); // pobierz aktualną wartosć przez radio
+    server.sendResponse(ip, port, packet.messageid, lamp, strlen(lamp), 
+                        packet.type, COAP_CONTENT, COAP_TEXT_PLAIN, packet.token, packet.tokenlen); // odeslij odpowiedź
+  }
+  else if (packet.code == COAP_PUT)
+  {
+    char p[packet.payloadlen + 1];
+    memcpy(p, packet.payload, packet.payloadlen);
+    p[packet.payloadlen] = NULL; // put null -> makes string
+    String message(p);
+    set_led(atoi(p)); // ustaw lampkę led na żądaną wartosć
+  }
+}
+
+// callback obsługujący żądania dotyczące klawiatury
 // Klient, aby stać się obserwatorem musi wysłać pakiet z opcją OBSERVE o wartości 0 i tak zostaje wpisany na listę obserwatorów, 
 // aby przestać obserwować zasób może wysłać wiadomość RST lub wiadomość z opcją Observe o wartości 1. 
 // Odpowiedź serwera następuje przy zmianie wartości zasobu, 
 // OBSERVE przyjmuje wartości sekwencji, token jest zawsze taki sam.
 void keyboard_callback(CoapPacket &packet, IPAddress ip, int port)
 {
-  String msg;
-  int observe = 0; // 0 - no option; 1 - observe; 2 - unobserve
-  int json = 0;
-  COAP_CONTENT_TYPE content_type = COAP_TEXT_PLAIN;
+  String msg; // tresć wiadomosci odpowiedzi
+  int observe = 0; // flaga pomocnicza (0 - no option; 1 - observe; 2 - unobserve)
+  int json = 0; // flaga pomocnicza opcji json
+  COAP_CONTENT_TYPE content_type = COAP_TEXT_PLAIN; // żądany typ reprezentacji zasobu
 
   conack_callback(packet, ip, port);
   
   if (packet.code == COAP_GET)
   {
-    get_keyboard(); //get current value over the radio
-    keyboard = (char) last_pressed;
-    
+    get_keyboard(); // pobierz aktualną wartosć przez radio
+
+    // dla każdej z opcji pakietu
     for (int i = 0; i < sizeof(packet.options); i++)
     { 
-      if (packet.options[i].number == COAP_OBSERVE) //observer
+      if (packet.options[i].number == COAP_OBSERVE) // sprawdź opcję observe
       {
-        if (*(packet.options[i].buffer) == 88) observe = 1;
-        else if (*(packet.options[i].buffer) == 1) observe = 2;
+        if (*(packet.options[i].buffer) == 88) observe = 1; // obserwuj
+        else if (*(packet.options[i].buffer) == 1) observe = 2; // przestań obserwować
       } 
       
-      if (packet.options[i].number == COAP_ACCEPT) // json
+      if (packet.options[i].number == COAP_ACCEPT) // sprawdź opcję accept
       {
         if (*(packet.options[i].buffer) == COAP_APPLICATION_JSON) 
         {
           json = 1;
-          content_type = COAP_APPLICATION_JSON;
+          content_type = COAP_APPLICATION_JSON; // odsyłaj wiadomosci o content-type json
         }
       }
     }
     
-    if (observe == 0)
+    if (observe == 0) // zwykły pakiet GET
     {
+      // konwersja wiadomosci w zależnosci od content-type
       if (json == 0) 
       {
+        // text/plain
         msg = String(keyboard);
       } 
       else if (json == 1) 
       {
+        // application/json
         msg = "{\"keyboard\": {\"key\": \"";
         msg += keyboard;
         msg += "\"}}";
@@ -285,25 +310,27 @@ void keyboard_callback(CoapPacket &packet, IPAddress ip, int port)
       server.sendResponse(ip, port, packet.messageid, msg.c_str(), strlen(msg.c_str()), 
                           packet.type, COAP_CONTENT, content_type, packet.token, packet.tokenlen);
     }
-    else if (observe == 1)
+    else if (observe == 1) // pakiet GET z funkcją Observe = 0
     {
       Serial.println("observe");
+
+      // dodaj nowego obserwatora
       our_observer.ip = ip;
       our_observer.port = port;
-      our_observer.counter = 2;
+      our_observer.counter = 2; // nadaj startowy numer sekwencyjny
       our_observer.content_type = (uint8_t) content_type;
       memcpy(our_observer.token, packet.token, packet.tokenlen);
       our_observer.tokenlen = packet.tokenlen;
-      notify();
+      notify(); // powiadom nowego obserwatora
     }
-    else if (observe == 2) 
+    else if (observe == 2) // pakiet GET z funkcją Observe = 1
     {
       // przestań obserwować zasób
       Serial.println("stop observing");
       our_observer.counter = -1;
     }  
   } 
-  else if (packet.code == COAP_RESET || observe == 2) 
+  else if (packet.code == COAP_RESET) // pakiet RST cofający obserwowanie zasobu
   {
     // przestań obserwować zasób
     Serial.println("stop observing");
@@ -311,6 +338,8 @@ void keyboard_callback(CoapPacket &packet, IPAddress ip, int port)
   }
 }
 
+// callback obsługujący żądania dotyczące statystyk pracy radia
+// metryki: średnie RTT, utrata pakietów
 void statistics_callback(CoapPacket &packet, IPAddress ip, int port)
 {
   conack_callback(packet, ip, port);
@@ -321,7 +350,7 @@ void statistics_callback(CoapPacket &packet, IPAddress ip, int port)
     payload_t* return_msg; // uchwyt na wiadomosc zwrotną
   
     unsigned long start_time, rtt, rtt_sum = 0;
-    short received = 0;
+    short received = 0; // ilość przekazanych z sukcesem wiadomosci
   
     Serial.println("Radio stats");
   
@@ -331,62 +360,57 @@ void statistics_callback(CoapPacket &packet, IPAddress ip, int port)
       bool success_receive = false;
   
       start_time = millis(); //rozpoczecie pomiaru czasu
-      success_send = radio_send_msg(STATS, 0);
+      success_send = radio_send_msg(STATS, 0); // spróbuj wysłać wiadomosć przez radio
   
       if (success_send)
       {
-        success_receive = radio_receive_msg(return_msg);
+        success_receive = radio_receive_msg(return_msg); // spróbuj odebrać odpowiedź przez radio
         if (success_receive) received++;
       }
   
       rtt = millis() - start_time; // obliczenie RTT
-      rtt_sum += rtt;
+      rtt_sum += rtt; // obliczenie sumarycznego RTT
     }
   
-    double packet_loss = ((5 - received) / 5) * 100;
-    double avg_rtt = rtt_sum / 5;
+    double packet_loss = ((5 - received) / 5) * 100; // procentowa utrata pakietów
+    double avg_rtt = rtt_sum / 5; // srednie RTT
     
     payload = String(received) + "/5 received, " + String(packet_loss) + "% packet loss, avg rtt " + String(avg_rtt);
     int payload_length = payload.length();
-    
+
+    // odpowiedź na żądanie o statystyki
     server.sendResponse(ip, port, packet.messageid, payload.c_str(), strlen(payload.c_str()), 
                         packet.type, COAP_CONTENT, COAP_TEXT_PLAIN, packet.token, packet.tokenlen);
     
-    delete return_msg;
+    delete return_msg; // zwalnianie pamięci
   }
 }
 
-void general_callback(CoapPacket &packet, IPAddress ip, int port)
-{
-  conack_callback(packet, ip, port);
-  
-  if (packet.code == COAP_GET)
-  {
-    server.sendResponse(ip, port, packet.messageid, well_known.c_str(), strlen(well_known.c_str()), 
-                        packet.type, COAP_CONTENT, COAP_APPLICATION_LINK_FORMAT, packet.token, packet.tokenlen);
-  }
-}
-
+// funkcja powiadamiająca obserwatora o zmianie stanu zasobu klawiatury
 void notify()
 {
-  if (our_observer.counter != -1)
+  if (our_observer.counter != -1) // sprawdż, czy ktos jest zarejestrowany jako obserwator
   {
-    String msg;
+    String msg; // tresć wiadomosci odpowiedzi
+
+    // w zależnosci od preferowanego przez obserwatora typu, wyslij mu powiadomienie w odpowiedniej reprezentacji
     if (our_observer.content_type == COAP_APPLICATION_JSON)
     {
-       msg = "{\"keyboard\": {\"key\": \"";
-       msg += keyboard;
-       msg += "\"}}";
+      // application/json
+      msg = "{\"keyboard\": {\"key\": \"";
+      msg += keyboard;
+      msg += "\"}}";
     }
     else
     {
-       msg = String(keyboard);  
+      // text/plain
+      msg = String(keyboard);  
     }
-        
+
     server.notifyObserver(our_observer.ip, our_observer.port, our_observer.counter, msg.c_str(), 
                           COAP_NONCON, COAP_CONTENT, our_observer.content_type,
-                          our_observer.token, our_observer.tokenlen);
-    our_observer.counter++;
+                          our_observer.token, our_observer.tokenlen); // wysłanie powiadomienia
+    our_observer.counter++; // zwiększ numer sekwencyjny dla kolejnego powiadomienia
   }
 }
 
@@ -394,6 +418,7 @@ void notify()
 // funkcje użytkowe
 //===========================================================================================================
 
+// funkcja służąca do zaglądania w opcje pakietu celem debuggowania
 /*void print_options(CoapPacket &packet)
 {
   Serial.print(String(packet.options[i].number));
