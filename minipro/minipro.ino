0a97c1b082b42a14bed58703029144bea612d6cf
Rafał Raczyński <toficer@gmail.com>
2020-01-05 16:35:49 +0100

    [minipro] rf24, requesty, lampka

--- minipro/minipro.ino
+++ minipro/minipro.ino
@@ -15,6 +15,8 @@
 //==========================================================================================================
 
 #include <SPI.h>
+#include <RF24Network.h>
+#include <RF24.h>
 #include <Ethernet.h>
 #include <EthernetUdp.h>
 #include <Keypad.h>
@@ -24,11 +26,11 @@
 //==========================================================================================================
 
 // Obsluga sieci
-byte mac[]={0x00, 0xaa, 0xbb, 0xcc, 0xde, 0xf3};
-EthernetUDP udp;
+//byte mac[]={0x00, 0xaa, 0xbb, 0xcc, 0xde, 0xf3};
+//EthernetUDP udp;
 char packetBuffer[255];
 int MAX_BUFFER_IN = 255;
-byte ip[] = { 10, 0, 0, 177 };
+//byte ip[] = { 10, 0, 0, 177 };
 
 
 // Oznaczenia literowe przycisków klawiatury
@@ -51,6 +53,34 @@ Keypad pad = Keypad(makeKeymap(keys), row, col, 4, 3);
 int LAMP_PIN = 3;
 int lampLightLevel = 255; // czyli nie swieci
 
+// Radio
+RF24 radio(9, 6);
+RF24Network network(radio);
+int THIS_NODE_ID = 1;
+int PEER_NODE_ID = 0;
+int OUR_CHANNEL = 47; // Do sprawdzenia?
+int currentMessageID = 1;
+
+// messageID zwiekszane przy kazdym wyslanym payload
+// resourceType okresli czy wysylamy lampke, klawiature czy statystyki
+// "data" moze byc stanem lampki lub reprezentacja liczbowa klawisza
+// trzeba jeszcze pomyslec nad tym jak wyslac statystyki...
+
+// z uno
+struct payload_t
+{
+  unsigned long timestamp; // stempel czasowy
+  short type; // typ zasobu
+  int value; // przenoszona wartość
+};
+
+// id zasobów do komunikacji z radiem
+const short ALL = 0;
+const short GET_KEYBOARD = 1;
+const short GET_LED = 2;
+const short SET_LED = 3;
+const short STATS = 4;
+
 //==========================================================================================================
 //                                                                                                     setup
 //==========================================================================================================
@@ -60,12 +90,10 @@ void setup() {
   // Serial dla debugu, do wywalenia w ostatecznej wersji
   Serial.begin(115200);
 
-  // Ethernet musi byc wywolany z danym IP, bo siec niekoniecznie jest
-  // w stanie podac nam je automatycznie (do weryfikacji?)
-  Ethernet.begin(mac, ip);
-  Serial.println(Ethernet.localIP());
-  short localPort = 2353;
-  udp.begin(localPort);
+  // Radio wywolywane na ustalonych pinach
+  SPI.begin();
+  radio.begin();
+  network.begin(OUR_CHANNEL, THIS_NODE_ID);
 
   // Output do sterowania poziomem lampki
   pinMode(LAMP_PIN, OUTPUT);
@@ -74,64 +102,24 @@ void setup() {
   setLightLevel(lampLightLevel);
 }
 
-//==========================================================================================================
-//                                                                                                      loop
-//==========================================================================================================
-
-void loop() {
-  
-  //getReqPickReaction();
-  getKeyAndLog(); // temp dla debugu
-  
-}
-
 //===========================================================================================================
 //                                                                                               inne funkcje
 //===========================================================================================================
 
+//============================================================================ Konwersja 0-1000 na 255-0 ====
+int levelToShort(int longLevel){
+  int shortLevel = longLevel/(1000/255);
+  return 255-shortLevel;
+}
 
-//====================================================================================== Obsluga zapytan ====
-void getReqPickReaction(){
-  
-  int size = udp.parsePacket();
-  Serial.println("req loop");
-  if (size) {
-
-    Serial.println("req loop size");
-    // Wstepne pobranie informacji o pakiecie
-    udp.read(packetBuffer, MAX_BUFFER_IN);
-    const char * reqType = getRequestType();
-    const char * device = getDeviceFromRequest();
-
-    // Reakcja na odpowiedni typ zapytania
-    if(reqType == "GET"){
-      // bla bla
-    }
-    else if(reqType ==  "PUT"){
-
-      // PUT musi zawierac jakas wartosc, raczej
-      const char * value = getValueFromRequest();
-
-      // Tylko LAMPKA obsluguje PUT (?)
-      if(device == "LAMPKA"){
-        int decValue = convertOctToDec(value);
-        setLightLevel(decValue);
-      }
-      else{
-        Serial.println("Nielegalne zadanie PUT, ignoruje.");
-        returnError();
-      }
-      
-    }
-    else {
-      Serial.println("Odebrano zapytanie nieznanego/nieobslugiwanego typu, ignoruje.");
-      returnError();
-    }
-  }
-  // else size=0, ignore, nie bede logowal zerowych
+//============================================================================ Konwersja 255-0 na 0-1000 ====
+int levelToLong(int shortLevel){
+  int longLevel = shortLevel*(1000/255);
+  return 1000-longLevel;
 }
 
 //============================================================== Pobieranie klawisza z klawiatury (+log) ====
+
 char getKeyAndLog() {
   
   char k = pad.getKey();
@@ -155,6 +143,41 @@ char getKeyAndLog() {
   return k;
 }
 
+//============================================ Pobieranie klawisza z klawiatury i wyslanie go przz radio ====
+char getKeyAndNotify() {
+  
+  char k = pad.getKey();
+
+  if(k != NO_KEY) {
+    Serial.print("SEND KEYPAD: ");
+    Serial.println(k);
+    RF24NetworkHeader header(PEER_NODE_ID);
+
+    struct payload_t p;
+    p.type = GET_KEYBOARD;
+    p.value = k;
+    p.timestamp = millis();
+    
+    bool success = network.write(header, &p, sizeof(p));
+    
+  }
+
+  return k;
+}
+
+//====================================================== Pobieranie stanu lampy i wyslanie go przez radio ====
+void getLightLevelAndNotify() {
+
+  RF24NetworkHeader header(PEER_NODE_ID);
+  struct payload_t p;
+  p.type = GET_LED;
+  p.value = levelToLong(lampLightLevel);
+  p.timestamp = millis();
+    
+  bool success = network.write(header, &p, sizeof(p));
+
+}
+
 //======================================================================= Konwersja z formatu OCT na DEC ====
 int convertOctToDec(const char value[]) {
   
@@ -173,43 +196,68 @@ int convertOctToDec(const char value[]) {
   return decValue;
 }
 
-//============================================================================ Pobieranie typu zapytania ====
-const char * getRequestType(){
-  // todo GET czy PUT, wyluskiwanie z req
-  return "PUT";
-}
-
-//==================================================================== Pobieranie czystej wartosci z PUT ====
-const char * getValueFromRequest(){
-  // obsluga wyluskiwania z pakietu CoAP czystej wartosci, jesli to PUT
-  return "100";
-}
-
-//================================== Pobieranie identyfikatora urzadzenia docelowego (klawiatura/lampka) ====
-const char * getDeviceFromRequest() {
-  //todo, odczyt czy lampka czy klawa
-  return "LAMPKA";
-}
-
 //=================================================================== Ustawianie stanu (jasnosci) lampki ====
 void setLightLevel(int level) {
   analogWrite(LAMP_PIN, level);
+  lampLightLevel = level;
 }
 
 //======================================================================= Zwroc blad przed lacze radiowe ====
 void returnError(){
-  // zwroc odpowiedni error code w zaleznosci od parametrow, todo
+  // walic radio, niech na ten moment po prostu wiem.
+  Serial.println("Nieobslugiwany typ requesta, odrzucam.");
 }
 
+//====================================================================================== Obsluga zapytan ====
+void getReqPickReaction(){
+
+  payload_t payload;
+  RF24NetworkHeader header(THIS_NODE_ID);
+  bool result = network.read(header, &payload, sizeof(payload));
+  
+  if (result) {
+
+    short reqType = payload.type;
+    int value = payload.value;
+    unsigned long timestamp = payload.timestamp;
+
+    // Reakcja na odpowiedni typ zapytania
+    if(reqType == GET_LED){
+      getLightLevelAndNotify();
+    }
+    else if(reqType == SET_LED){
+      int decValue = convertOctToDec(value);
+      setLightLevel(levelToShort(decValue));
+    }
+    else if(reqType == GET_KEYBOARD){
+      getKeyAndNotify();
+    }
+    else if(reqType == STATS){
+      payload.timestamp = millis();
+      bool success = network.write(header, &payload, sizeof(payload));
+    }
+    else {
+      Serial.println("Odebrano zapytanie nieznanego/nieobslugiwanego typu, ignoruje.");
+      returnError();
+    }
+  }
+}
+
+//==========================================================================================================
+//                                                                                                      loop
+//==========================================================================================================
+
+void loop() {
+
+  network.update();
+  while (network.available()){
+    getReqPickReaction();
+  }
+  
+  getKeyAndLog(); // temp dla debugu
+  
+}
 
 //===========================================================================================================
 //                                                                                            K O S T N I C A
 //===========================================================================================================
-
-
-
-        // potencjalnie uzyteczne
-        //
-        //udp.beginPacket(udp.remoteIP(), udp.remotePort());
-        //int l = udp.write(sendBuffer, MAX_BUFFER_OUT);
-        //udp.endPacket();
