b4d5979f7ee6c62ba384fcf225e9285711494019
Rafał Raczyński <toficer@gmail.com>
2020-01-15 15:04:57 +0100

    Wersja w pełni sprawna, nieokomentowana, z debugiem (serial.println)

--- minipro/minipro.ino
+++ minipro/minipro.ino
@@ -1,15 +1,4 @@
-// Mini Pro
-// Lampka + klawiatura
-//
-// z pobudek religijnych odrzucam snejka
-// nie jestem w stanie tego przetestowac poza kompilacja
-//
-// (a ze guzik z C pamietam, pewnie eksploduje)
-//
-// ni w zab nie ogarniam tych char arrayow w funkcjach w C,
-// chca jakichs cost char pointerow
-// bo niby z C++ niekompatybilne...
-//
+
 //==========================================================================================================
 //                                                                                                   includy
 //==========================================================================================================
@@ -17,21 +6,11 @@
 #include <SPI.h>
 #include <RF24Network.h>
 #include <RF24.h>
-//#include <Ethernet.h>
-//#include <EthernetUdp.h>
 #include <Keypad.h>
 
 //==========================================================================================================
 //                                                                                                deklaracje
 //==========================================================================================================
-
-// Obsluga sieci
-//byte mac[]={0x00, 0xaa, 0xbb, 0xcc, 0xde, 0xf3};
-//EthernetUDP udp;
-char packetBuffer[255];
-int MAX_BUFFER_IN = 255;
-//byte ip[] = { 10, 0, 0, 177 };
-
 
 // Oznaczenia literowe przycisków klawiatury
 char keys[4][3] = {
@@ -43,8 +22,8 @@ char keys[4][3] = {
 
 
 // Piny na rzędy i kolumny klawiatury:
-byte row[4] = {4, 5, A0, 2};
-byte col[3] = {A2, A1, A3};
+byte row[4] = {7, 9, A3, 5};
+byte col[3] = {6, 4, 8};
 
 // Klawiatura
 Keypad pad = Keypad(makeKeymap(keys), row, col, 4, 3);
@@ -54,14 +33,13 @@ int LAMP_PIN = 3;
 int lampLightLevel = 255; // czyli nie swieci
 
 // Radio
-RF24 radio(9, 6);
+RF24 radio(A2, 10);
 RF24Network network(radio);
-int THIS_NODE_ID = 1;
-int PEER_NODE_ID = 0;
-int OUR_CHANNEL = 47; // Do sprawdzenia?
-int currentMessageID = 1;
+const short THIS_NODE_ID = 1;
+const short PEER_NODE_ID = 0;
+int OUR_CHANNEL = 47;
 
-// z uno
+// Payload radiowy
 struct payload_t
 {
   unsigned long timestamp; // stempel czasowy
@@ -70,11 +48,11 @@ struct payload_t
 };
 
 // id zasobów do komunikacji z radiem
-const short ALL = 0;
+const short ALL = 0; // nieuzywane?
 const short GET_KEYBOARD = 1;
 const short GET_LED = 2;
 const short SET_LED = 3;
-const short STATS = 4;
+const short STATS = 4; // nieuzywane?
 
 //==========================================================================================================
 //                                                                                                     setup
@@ -95,6 +73,8 @@ void setup() {
 
   // Setup poziomu jasnosci na poczatkowy
   setLightLevel(lampLightLevel);
+
+  Serial.println("SETUP done");
 }
 
 //===========================================================================================================
@@ -103,18 +83,23 @@ void setup() {
 
 //============================================================================ Konwersja 0-1000 na 255-0 ====
 int levelToShort(int longLevel){
-  int shortLevel = longLevel/(1000/255);
+  int shortLevel =(int) ((float)longLevel/(1000.0/255.0));
   return 255-shortLevel;
 }
 
 //============================================================================ Konwersja 255-0 na 0-1000 ====
 int levelToLong(int shortLevel){
-  int longLevel = shortLevel*(1000/255);
+  int longLevel = (int) ((float)shortLevel*(1000.0/255.0));
   return 1000-longLevel;
 }
 
 //============================================================== Pobieranie klawisza z klawiatury (+log) ====
+//==============================================================                  CZASEM PRZYDATNY DEBUG ====
+//==============================================================              DO WYWALENIA JAK SKONCZYMY ====
 
+// Ustawia poziom lampki jako klawisz*51 (w notacji 0-255)
+// + Wypisuje klawisz
+// + * Wypisuje poziom
 char getKeyAndLog() {
   
   char k = pad.getKey();
@@ -131,14 +116,14 @@ char getKeyAndLog() {
       setLightLevel(tempLightLevel);
       lampLightLevel = tempLightLevel;
       Serial.print("DEBUG set light level: ");
-      Serial.println(lampLightLevel);
+      Serial.println(levelToLong(lampLightLevel));
     }
   }
 
   return k;
 }
 
-//============================================ Pobieranie klawisza z klawiatury i wyslanie go przz radio ====
+//============================================ Pobieranie klawisza z klawiatury i wyslanie go przez radio ====
 char getKeyAndNotify() {
   
   char k = pad.getKey();
@@ -154,6 +139,12 @@ char getKeyAndNotify() {
     p.timestamp = millis();
     
     bool success = network.write(header, &p, sizeof(p));
+    if(success){
+      Serial.println("KEY sent");
+    }
+    else {
+      Serial.println("KEY fail");
+    }
     
   }
 
@@ -164,55 +155,53 @@ char getKeyAndNotify() {
 void getLightLevelAndNotify() {
 
   RF24NetworkHeader header(PEER_NODE_ID);
-  struct payload_t p;
+  payload_t p;
   p.type = GET_LED;
   p.value = levelToLong(lampLightLevel);
   p.timestamp = millis();
     
   bool success = network.write(header, &p, sizeof(p));
-
-}
-
-//======================================================================= Konwersja z formatu OCT na DEC ====
-int convertOctToDec(const char value[]) {
-  
-  int octValue = atoi(value);
-  int decValue = 1;
-  int i = 0;
-  int temp = 0;
-
-  while (octValue != 0) {
-    temp = octValue % 10;
-    octValue /= 10;
-    decValue += temp * pow(8, i);
-    i++;
+  if(success){
+    Serial.println("LIGHT sent");
+  }
+  else {
+    Serial.println("LIGHT fail");
   }
 
-  return decValue;
 }
 
 //=================================================================== Ustawianie stanu (jasnosci) lampki ====
 void setLightLevel(int level) {
+  Serial.print("SET LIGHT INTERNAL: ");
   Serial.println(level);
   analogWrite(LAMP_PIN, level);
+  
   lampLightLevel = level;
 }
 
-//======================================================================= Zwroc blad przed lacze radiowe ====
-void returnError(){
-  // walic radio, niech na ten moment po prostu wiem.
-  Serial.println("Nieobslugiwany typ requesta, odrzucam.");
+//====================================================================================== Obsluga zapytan ====
+bool getReq(payload_t *payload){
+
+  RF24NetworkHeader header(THIS_NODE_ID);
+  bool result = network.read(header, payload, sizeof(*payload));
+  return result;
 }
 
-//====================================================================================== Obsluga zapytan ====
-void getReqPickReaction(){
+//==========================================================================================================
+//                                                                                                      loop
+//==========================================================================================================
 
+void loop() {
+
+  bool getReqSuccess = false;
+  network.update();
   payload_t payload;
-  RF24NetworkHeader header(THIS_NODE_ID);
-  bool result = network.read(header, &payload, sizeof(payload));
   
-  if (result) {
+  while (network.available()){
+    getReqSuccess = getReq(&payload);
+  }
 
+  if(getReqSuccess){
     short reqType = payload.type;
     int value = payload.value;
     unsigned long timestamp = payload.timestamp;
@@ -222,36 +211,20 @@ void getReqPickReaction(){
       getLightLevelAndNotify();
     }
     else if(reqType == SET_LED){
-      int decValue = convertOctToDec(value);
-      setLightLevel(levelToShort(decValue));
+      setLightLevel(levelToShort(value));
     }
     else if(reqType == GET_KEYBOARD){
       getKeyAndNotify();
     }
-    else if(reqType == STATS){
-      payload.timestamp = millis();
-      bool success = network.write(header, &payload, sizeof(payload));
-    }
     else {
       Serial.println("Odebrano zapytanie nieznanego/nieobslugiwanego typu, ignoruje.");
-      returnError();
     }
   }
-}
 
-//==========================================================================================================
-//                                                                                                      loop
-//==========================================================================================================
+  // Jesli radio milczy - sprawdz, czy nie klika ktos przycisku.
+  getKeyAndNotify();
 
-void loop() {
-
-  network.update();
-  while (network.available()){
-    Serial.println("DEBUG avail");
-    getReqPickReaction();
-  }
-  
-  getKeyAndLog(); // temp dla debugu
+  //getKeyAndLog(); // Debug na wypadek testowania klawiatury bez radia
   
 }
 
